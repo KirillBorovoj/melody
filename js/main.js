58142a6cb79e41cb9173499e026a8411ccf03740
KirillBorovoj <39945848+KirillBorovoj@users.noreply.github.com>
2021-08-24 23:07:49 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,33 @@
  $(document).ready(function () {
-   var currentFloor = 2;
-   var floorPath = $(".home-image path");
-   var counterUp = $(".counter-up");
-   var counterDown = $(".counter-down");
-   floorPath.on("mouseover", function () {
-     floorPath.removeClass("current-floor");
-     currentFloor = $(this).attr('data-floor');
-     $(".counter").text(currentFloor);
-   });
+  var currentFloor = 2;
+  var floorPath = $(".home-image path");
+  var counterUp = $(".counter-up");
+  var counterDown = $(".counter-down");
+  var modal = $(".modal");
+  var modalCloseButton = $(".modal-close-button");
+  var viewFlatsButton = $(".view-flats");
 
-   counterUp.on("click", function(){
-     if (currentFloor < 18) {
+  //функция при наведении мышью на этаж
+  floorPath.on("mouseover", function () {
+    floorPath.removeClass("current-floor");
+    currentFloor = $(this).attr('data-floor');
+    $(".counter").text(currentFloor);
+  });
+
+  floorPath.on('click', function(){
+    modal.toggleClass("is-open");
+  });
+
+  modalCloseButton.on('click', function(){
+    modal.toggleClass("is-open");
+  });
+
+  viewFlatsButton.on('click', function(){
+    modal.toggleClass("is-open");
+  });
+
+  counterUp.on("click", function(){
+    if (currentFloor < 18) {
       currentFloor++;
       usCurrentFloor = currentFloor.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false});
       $(".counter").text(usCurrentFloor);
@@ -19,7 +36,7 @@
       }
   })
     counterDown.on("click", function(){
-      if (currentFloor < 2) {
+      if (currentFloor > 2) {
         currentFloor--;
         usCurrentFloor = currentFloor.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false});
         $(".counter").text(usCurrentFloor);
